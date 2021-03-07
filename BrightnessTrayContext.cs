8000dbf403abf50c5c84d5083cab14e94f8a2816
David Blain <drblain22@gmail.com>
2021-03-07 10:52:07 -0500

    Initial commit

---
+++ BrightnessTrayContext.cs
@@ -0,0 +1,55 @@
+﻿using BrightAdjust.Properties;
+using OpenCvSharp;
+using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BrightAdjust
+{
+    class BrightnessTrayContext : SysTrayAppContext
+    {
+        public BrightnessTrayContext()
+        {
+            this.TrayIcon.Icon = Resources.SmallIcon;
+
+            this.ContextMenu.Items.Add("Adjust &Brightness", null, this.AdjustBrightnessContextMenuHandler).Font = new Font(ContextMenu.Font, FontStyle.Bold);
+            this.ContextMenu.Items.Add("-");
+            this.ContextMenu.Items.Add("E&xit", null, this.ExitContextMenuClickHandler);
+        }
+
+        protected override void OnTrayIconDoubleClicked(MouseEventArgs e)
+        {
+            this.HandleChangeBrightness();
+
+            base.OnTrayIconDoubleClicked(e);
+        }
+
+        private void AdjustBrightnessContextMenuHandler(object sender, EventArgs e)
+        {
+            this.HandleChangeBrightness();
+        }
+
+        private void ExitContextMenuClickHandler(object sender, EventArgs e)
+        {
+            this.ExitThread();
+        }
+
+        private void HandleChangeBrightness()
+        {
+            // The argument to change brightness is the number of threads to use during execution
+            this.ChangeBrightness(4);
+        }
+
+        private void ChangeBrightness(int numThreads)
+        {
+            // Capture an image from the primary camera
+            VideoCapture cam = new VideoCapture(0);
+            Mat img = new Mat();
+            cam.Read(img);
+            cam.Release();
+            Console.WriteLine(img);
+        }
+
+    }
+}
---
+++ SysTrayAppContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BrightAdjust
+{
+    class SysTrayAppContext : ApplicationContext
+    {
+        private readonly NotifyIcon _notifyIcon;
+        private readonly ContextMenuStrip _contextMenu;
+
+        protected SysTrayAppContext()
+        {
+            Application.ApplicationExit += this.ApplicationExitHandler;
+
+            _contextMenu = new ContextMenuStrip();
+            _notifyIcon = new NotifyIcon
+            {
+                ContextMenuStrip = _contextMenu,
+                Text = Application.ProductName, 
+                Visible = true 
+            };
+
+            this._notifyIcon.MouseDoubleClick += this.IconDoubleClickHandler;
+            this._notifyIcon.MouseClick += this.IconClickHandler;
+        }
+
+        protected ContextMenuStrip ContextMenu
+        {
+            get { return _contextMenu;  }
+        }
+
+        protected NotifyIcon TrayIcon
+        {
+            get { return _notifyIcon; }
+        }
+
+        protected virtual void OnTrayIconClicked(MouseEventArgs e)
+        {
+            
+        }
+
+        protected virtual void OnTrayIconDoubleClicked(MouseEventArgs e)
+        {
+
+        }
+
+        private void IconClickHandler(object sender, MouseEventArgs e)
+        {
+            this.OnTrayIconClicked(e);
+        }
+
+        private void IconDoubleClickHandler(object sender, MouseEventArgs e)
+        {
+            this.OnTrayIconDoubleClicked(e);
+        }
+
+        protected virtual void OnApplicationExit(EventArgs e)
+        {
+            // dispose of notification icon
+            _notifyIcon.Visible = false;
+            _notifyIcon.Dispose();
+            // dispose of the context menu
+            if (_contextMenu != null)
+            {
+                _contextMenu.Dispose();
+            }
+        }
+
+        private void ApplicationExitHandler(object sender, EventArgs e)
+        {
+            this.OnApplicationExit(e);
+        }
+    }
+}
